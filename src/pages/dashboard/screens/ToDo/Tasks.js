4ba7803d144268b6df191ca45c40ec01e93065cf
MooCowGalaxy <64293406+MooCowGalaxy@users.noreply.github.com>
2022-07-28 18:23:12 -0700

    add message when all tasks are completed

--- src/pages/dashboard/screens/ToDo/Tasks.js
+++ src/pages/dashboard/screens/ToDo/Tasks.js
@@ -46,12 +46,12 @@ function GetTasks(prop) {
 	}, [autoAuthReq, flashMsg, orderId]);
 	
 	// create toDo Tasks containers 
-	const toDoTasks = tasksList.map((task) =>{
+	const toDoTasks = tasksList.map((task) => {
 		if (!comTasks.includes(task.id)) {
 			return (
 				<label className="containertask" key={task.id} >{task.text}
-					<input type="checkbox" value = {String(task.id) + ',' + String(orderId)} onClick={onClick}></input>
-					<span className="checkmark"></span>
+					<input type="checkbox" value = {String(task.id) + ',' + String(orderId)} onClick={onClick} />
+					<span className="checkmark" />
 					<p className='text-sm text-red-500 italic'>Deadline: {task.deadline}</p>
 				</label>
 			);
@@ -59,12 +59,14 @@ function GetTasks(prop) {
 		return null;
 	});
 
-	return (
-		<>
-			{toDoTasks}
-		</>
-	);
+	let list;
+	if (tasksList.length > 0) {
+		list = toDoTasks;
+	} else {
+		list = <p className="text-center w-full text-lg">All tasks are completed!</p>;
+	}
 
+	return list;
 }
 
 export default GetTasks;
--- src/pages/dashboard/screens/ToDo/ToDo.js
+++ src/pages/dashboard/screens/ToDo/ToDo.js
@@ -35,15 +35,15 @@ function ToDo() {
 		for (let order of orders) {
 			// For each order, create Task Container for each order with both Tasks and Completed Tasks
 			orderList.push((
-           	<div className="container flex flex-wrap mx-auto mt-10 px-5 mb-10">           
-			   <div className="flex-none md:flex-initial w-full md:w-full py-5 px-8 bg-white text-black rounded-t-xl md:rounded-t-xl md:rounded-none">
-				   <h1 className="text-2xl mb-8 text-center">{order.user.first_name} {order.user.last_name}'s To Do List</h1>
-				   <form>
-					   {isLoaded ? <Loading></Loading> : null}
-					   <GetTasks orderId ={order.id} comTasks = {order.completed_tasks}></GetTasks>
+				<div className="container flex flex-wrap mx-auto mt-10 px-5 mb-10">
+				   <div className="flex-none md:flex-initial w-full md:w-full py-5 px-8 bg-white text-black rounded-t-xl md:rounded-t-xl md:rounded-none">
+					   <h1 className="text-2xl mb-8 text-center">{order.user.first_name} {order.user.last_name}'s To Do List</h1>
+					   <form>
+						   {isLoaded ? <Loading /> : null}
+						   <GetTasks orderId={order.id} comTasks={order.completed_tasks} />
 					   </form>
 					</div>
-					<GetCompletedTasks orderId ={order.id} comTasks = {order.completed_tasks}></GetCompletedTasks>
+					<GetCompletedTasks orderId={order.id} comTasks={order.completed_tasks} />
 				</div>
 			));
 		}
