e4c2eee5f078cda600159a232c4fb21bd5f43403
MooCowGalaxy <64293406+MooCowGalaxy@users.noreply.github.com>
2022-07-28 17:09:48 -0700

    update code to use new API format

--- src/pages/dashboard/screens/courseDetails.js
+++ src/pages/dashboard/screens/courseDetails.js
@@ -20,7 +20,7 @@ function DashboardCourseDetails({ setIsStudentRegistered }) {
 				baseUrl + `/api/v1/users/${auth.user.id}/orders/`, 
 				{ method: 'GET' }
 			);
-			const scholarshipData = data.data.filter((order) => order.status === 'UNPAID' && order.scholarship.name !== null);
+			const scholarshipData = data.data.filter((order) => order.scholarship !== undefined);
 			setScholarships(scholarshipData);
 
 			// check if user is student and user has paid course
@@ -34,8 +34,8 @@ function DashboardCourseDetails({ setIsStudentRegistered }) {
 			// fetch incomplete tasks from API
 			const courseTaskDict = {};
 			for (let course of data.data) {
-				if (scholarshipData.filter(order => order.id === course.id).length > 0) continue; // remove duplicates
-				courseTaskDict[course.id] = (await auth.autoAuthReq(baseUrl + `/api/v1/orders/${course.id}/tasks/?countonly=true`, { method: 'GET' })).data.count > 0;
+				if (scholarshipData.filter(order => order.order.id === course.order.id).length > 0) continue; // remove duplicates
+				courseTaskDict[course.order.id] = (await auth.autoAuthReq(baseUrl + `/api/v1/orders/${course.order.id}/tasks/?countonly=true`, { method: 'GET' })).data.count > 0;
 			}
 
 			setCourseTasks(courseTaskDict);
@@ -50,7 +50,6 @@ function DashboardCourseDetails({ setIsStudentRegistered }) {
 	if (courseTasks !== null) {
 		coursesList = [];
 		for (let course of courses) {
-			// TODO: remove dummy deadline - API does not return a deadline yet
 			const transactionsTable = course.transactions.map((transaction, index) => (
 				<div className='grid grid-cols-4 gap-4 text-center text-xs md:text-base pb-2' key={index}>
 					<div>{transaction.created_at.substring(0, 10)}</div>
@@ -65,27 +64,27 @@ function DashboardCourseDetails({ setIsStudentRegistered }) {
 				</div>
 			));
 
-			const scholarship = course.scholarship.status !== null && course.scholarship.status.startsWith('ACCEPTED_') ? (
+			const scholarship = course.scholarship.name !== null && course.scholarship.status.startsWith('ACCEPTED_') ? (
 				<p className="mb-5"><b className="font-semibold">Scholarship Status: </b><span className="text-green-500">{course.scholarship.name}</span></p>
 			) : null;
 			const isFullScholarship = course.scholarship.status === 'ACCEPTED_FULL';
 
 			coursesList.push((
-				<div key={course.id} className="container flex flex-wrap mx-auto mt-12 px-6 pb-6">
+				<div key={course.order.id} className="container flex flex-wrap mx-auto mt-12 px-6 pb-6">
 					<div className={`flex-none md:flex-initial w-full md:w-7/12 py-8 px-16 pb-10 text-lg text-black bg-white rounded-t-xl ${isFullScholarship ? 'rounded-t-xl md:rounded-l-xl md:rounded-none' : 'md:rounded-tl-xl md:rounded-none'}`}>
 						<h1 className="font-semibold text-2xl mb-8 text-center">Course Information</h1>
 						<p className="mb-5"><b className="font-semibold">Student Name: </b>{course.user.first_name} {course.user.last_name}</p>
 						<p><b className="font-semibold">Course: </b>{course.course.name}</p>
 						<p><b className="font-semibold">{course.batch.name}: </b>{course.batch.start_date} - {course.batch.end_date}, {course.batch.start_time} - {course.batch.end_time} {course.batch.time_zone}</p>
-						<p className="mb-5"><a href={`/orders/${course.id}/change-batch`} className="text-blue-500 underline">Want to change your batch?</a><span className="italic text-red-400"> (Deadline: {course.course.batch_change_deadline})</span></p>
+						<p className="mb-5"><a href={`/orders/${course.order.id}/change-batch`} className="text-blue-500 underline">Want to change your batch?</a><span className="italic text-red-400"> (Deadline: {course.course.batch_change_deadline})</span></p>
 						{scholarship}
-						<Link to={`/orders/${course.id}/refund`} className='mb-4'>
+						<Link to={`/orders/${course.order.id}/refund`} className='mb-4'>
 							<Button bgColor="white" txtColor="black" className="w-2/3 py-1">Cancel Your Course</Button>
 						</Link>
 					</div>
 					<div className={`flex-none md:flex-initial w-full md:w-5/12 py-8 px-16 pb-10 bg-stone-300 ${isFullScholarship ? 'rounded-b-xl md:rounded-tr-xl md:rounded-bl-none' : 'md:rounded-tr-xl md:rounded-none'}`}>
 						<h1 className="font-semibold text-2xl mb-10 text-center">Course Materials</h1>
-						{courseTasks[course.id] ? 
+						{courseTasks[course.order.id] ?
 							<Link to = 'todo'>
 								<Button bgColor="white" txtColor="black" className="w-full py-3 mb-4">Student To Do List</Button>
 							</Link> :
@@ -125,6 +124,7 @@ function DashboardCourseDetails({ setIsStudentRegistered }) {
 				);
 			} else if (scholarships.length > 0) {
 				const scholarshipList = [];
+
 				let hasAcceptedScholarship = false;
 				let registerForCourses = false;
 
@@ -137,7 +137,7 @@ function DashboardCourseDetails({ setIsStudentRegistered }) {
 						const scholarshipText = scholarshipOrder.scholarship.name;
 
 						scholarshipList.push((
-							<div className="container flex flex-wrap mx-auto mb-10 px-6 pb-6" key={scholarshipOrder.id}>
+							<div className="container flex flex-wrap mx-auto mb-10 px-6 pb-6" key={scholarshipOrder.order.id}>
 								<div className="flex-none md:flex-initial w-full md:w-7/12 py-8 px-16 pb-10 text-lg text-black bg-white rounded-t-xl md:rounded-l-xl md:rounded-none">
 									<h1 className="font-semibold text-2xl mb-8 text-center">Course Information</h1>
 									<p className="mb-5"><b className="font-semibold">Student Name: </b>{scholarshipOrder.user.first_name} {scholarshipOrder.user.last_name}</p>
@@ -157,7 +157,7 @@ function DashboardCourseDetails({ setIsStudentRegistered }) {
 
 						if (!hasAcceptedScholarship) registerForCourses = true;
 						scholarshipList.push((
-							<div className="container flex flex-wrap mx-auto mb-10 px-6 pb-6 mb-8">
+							<div className="container flex flex-wrap mx-auto mb-10 px-6 pb-6 mb-8" key={scholarshipOrder.order.id}>
 								<div className="flex-none w-full py-8 px-16 pb-10 text-lg text-black bg-white rounded-xl">
 									<h1 className="font-semibold text-2xl mb-8 text-center">Course Information</h1>
 									<p className="mb-5"><b className="font-semibold">Student Name: </b>{scholarshipOrder.user.first_name} {scholarshipOrder.user.last_name}</p>
